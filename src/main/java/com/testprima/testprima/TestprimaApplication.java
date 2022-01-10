cec7ae1b688f6e78d9813e168d570ca14936a3b1
suryadi sukisna <suryadisukisna@gmail.com>
2022-01-10 10:04:55 +0700

    added main application

---
+++ src/main/java/com/testprima/testprima/TestprimaApplication.java
@@ -0,0 +1,12 @@
+package com.testprima.testprima;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestprimaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestprimaApplication.class, args);
+	}
+} 
